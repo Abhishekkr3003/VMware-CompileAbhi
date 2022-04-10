b4e020d26e673e3e5e1a0dff8d7338ecccd11f9f
Abhinav Jain <abhinavjain@Abhinavs-MacBook-Air.local>
2022-04-10 18:37:55 +0530

    need login persistence

--- frontend/src/firebase/firebase.utils.js
+++ frontend/src/firebase/firebase.utils.js
@@ -1,6 +1,9 @@
 import firebase from "firebase/compat/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import "firebase/firestore";
 import "firebase/compat/auth";
+import { setUser } from "../actions/index";
+import { useDispatch } from "react-redux";
 
 // import "firebase/firestore";
 // import "firebase/auth";
@@ -17,13 +20,34 @@ const firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 
-export const auth = firebase.auth();
+const auth = getAuth();
 // export const firestore = firebase.firestore();
 
-const provider = new firebase.auth.GoogleAuthProvider();
+const provider = new GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
-export const signInWithGoogle = () => auth.signInWithPopup(provider);
-export default firebase;
+export const signInWithGoogle = async () => {
+  return await signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      return user;
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+};
+// export default firebase;
 
 // export const auth = firebase.auth();
 // const googleProvider = new firebase.auth.GoogleAuthProvider();
